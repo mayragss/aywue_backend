de428b1b3da1f4b5c941206e3a95c461d02ea3b0
mayragsss <mayra.g.dias@hotmail.com>
2025-05-19 23:15:17 +0100

    new api key

--- config/context.js
+++ config/context.js
@@ -2,7 +2,7 @@ const sql = require("mysql2/promise");
 
 // Configuração do banco de dados
 const dbConfig = {
-  host: '82.25.93.13', 
+  host: 'localhost', 
   user: 'root',
   password: 'aywWjo323=*',
   database: 'AYWUEDEV',
